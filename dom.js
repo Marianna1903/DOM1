5b6908027d06e36d1c693c0a702ba47f9c8e0766
Marianna1903 <84705083+Marianna1903@users.noreply.github.com>
2021-10-08 15:43:22 +0400

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,67 @@
+//1
+// let table = document.getElementById("table");
+// let tr = "";
+// for(var i=1;i<=10;i++){
+// 	tr += "<tr>";
+// 	for(j=1;j<=10;j++){
+// 		tr += "<td>"+i*j+"</td>";
+// 	}
+// 	tr += "</tr>"
+// }
+// table.innerHTML = tr;
+
+
+//2
+// let table = document.getElementById("table");
+// let tr = "";
+// for(var i=0;i<10;i++){
+// 	tr += "<tr>";
+// 	for(var j=0;j<10;j++){
+// 		if(i>0 && j>0 && i<9 && j<9){
+// 			tr += "<td style='background-color: white'></td>";
+// 		}else{
+// 			tr += "<td style='background-color:"+"rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")"+"'></td>";
+// 		}
+// 	}
+// 	tr += "</tr>";
+// }
+// table.innerHTML = tr;
+
+
+//3
+// let table = document.getElementById("table");
+// let tr = "";
+// for(var i=0;i<10;i++){
+// 	tr += "<tr>";
+// 	for(var j=0;j<10;j++){
+// 		if(i>0 && j>0 && i<9 && j<9){
+// 			tr += "<td style='background-color:"+"rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")"+"'></td>";
+// 		}else{
+// 			tr += "<td style='background-color: white'></td>";
+// 		}
+// 	}
+// 	tr += "</tr>";
+// }
+// table.innerHTML = tr;
+
+
+
+////funkciayov
+
+// function rgb(){
+// 	return Math.floor(Math.random()*255);
+// }
+
+// let tr = "";
+// for(var i=0;i<10;i++){
+// 	tr += "<tr>";
+// 	for(var j=0;j<10;j++){
+// 		if(i>0 && j>0 && i<9 && j<9){
+// 			tr += "<td style='background-color:"+"rgb("+rgb()+","+rgb()+","+rgb()+")"+"'></td>";
+// 		}else{
+// 			tr += "<td style='background-color: white'></td>";
+// 		}
+// 	}
+// 	tr += "</tr>";
+// }
+// table.innerHTML = tr;
